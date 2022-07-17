0bb27ab79d6aa65d9f114c8de01262224e14db22
Omar Jaff <me@omarjaff.com>
2022-07-18 01:32:08 +0300

    followed the controller namespace folder convenstion

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,21 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', [PostController::class, 'index'] )->name('home');
 
 
-
-
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+
+Route::get('/author/{author:username}', function (User $author) {
+   return view('posts', [
+       'posts' => $author->posts
+   ]) ;
+});
+
+
 
 
 //Route::get('/category/{category:slug}', function (Category $category) {
@@ -32,10 +30,3 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
 //        'currentCategory' => $category
 //    ]);
 //});
-
-
-Route::get('/author/{author:username}', function (User $author) {
-   return view('posts', [
-       'posts' => $author->posts
-   ]) ;
-});
