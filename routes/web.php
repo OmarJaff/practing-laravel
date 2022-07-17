402b989d9a9f38bce8cf76c4275447fe1cc1f075
Omar Jaff <me@omarjaff.com>
2022-07-17 21:25:53 +0300

    moved route logic from the clouser to the controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -16,18 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' =>Category::all(),
-     ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'] )->name('home');
 
-Route::get('/posts/{post}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+
+
+
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 
 Route::get('/category/{category:slug}', function (Category $category) {
@@ -37,6 +32,7 @@ Route::get('/category/{category:slug}', function (Category $category) {
         'currentCategory' => $category
     ]);
 });
+
 
 Route::get('/author/{author:username}', function (User $author) {
    return view('posts', [
