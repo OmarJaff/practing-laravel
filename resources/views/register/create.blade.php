1942d8c40b2a7ef979e8697a046017d4a923311f
Omar Jaff <me@omarjaff.com>
2022-07-22 23:18:27 +0300

    latest

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,54 @@
+<x-layout>
+
+     <main class="max-w-lg mx-auto my-16">
+         <h1 class="my-2 font-semi-bold text-center">Register</h1>
+
+         <form method="POST" action="/register" class="rounded border-gray-100 bg-gray-50 p-4">
+            @csrf
+             <div class="mb-6">
+                     <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+                            for="name">
+                            Name
+                     </label>
+                     <input class="w-full p-2 border border-gray-400 rounded"
+                            type="text" name="name"
+                       required>
+             </div>
+
+             <div class="mb-6">
+                 <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+                        for="username">
+                     User Name
+                 </label>
+                 <input class="w-full p-2 border border-gray-400 rounded"
+                        type="text" name="username"
+                        required>
+             </div>
+
+             <div class="mb-6">
+                 <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+                        for="email">
+                     Email Address
+                 </label>
+                 <input class="w-full p-2 border border-gray-400 rounded"
+                        type="email" name="email"
+                        required>
+             </div>
+
+             <div class="mb-6">
+                 <label class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+                        for="password">
+                     Password
+                 </label>
+                 <input class="w-full p-2 border border-gray-400 rounded"
+                        type="password" name="password"
+                        required>
+             </div>
+
+                <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded">
+                    Submit
+                </button>
+         </form>
+     </main>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Support\Facades\Route;
+ use Illuminate\Support\Facades\Route;
 
 
 
@@ -12,4 +13,7 @@ Route::get('/', [PostController::class, 'index'] )->name('home');
 
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
+Route::get('/register', [RegisterController::class, 'create']);
+
+Route::post('/register', [RegisterController::class, 'store']);
 
