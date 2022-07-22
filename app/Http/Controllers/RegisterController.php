5bf2b77dc2290d81bb846e9cd0cc621957aa1698
Omar Jaff <me@omarjaff.com>
2022-07-23 02:05:09 +0300

    Applied validation for username and email

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,8 +17,8 @@ class RegisterController extends Controller
     {
         $attributes = request()->validate([
             'name' => 'required|string|max:255',
-            'username' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
+            'username' => 'required|string|min:4|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
             'password' => ['required', Password::min(8)->letters()->numbers()]
     ]);
 
