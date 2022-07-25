a14d8e62579376af5a1d07290c60d48cea6564b9
Omar Jaff <me@omarjaff.com>
2022-07-25 21:35:33 +0300

    latest

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -2,7 +2,7 @@
 
 <article class="flex space-x-6 bg-gray-100 rounded-xl border border-gray-200 px-4 py-5">
     <div class="flex-shrink-0">
-        <img class="rounded w-16 h-50" src="https://i.pravatar.cc/100?u={{$comment->id}}" alt="avatar">
+        <img class="rounded w-16 h-50" src="https://i.pravatar.cc/100?u={{$comment->author->id}}" alt="avatar">
     </div>
     <div class="space-y-2">
         <header>
